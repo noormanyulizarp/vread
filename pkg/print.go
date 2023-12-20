d32b840bbef64089f3ea94f556623bd3b2ba4af8
noormanyulizarp <noormanyulizarp@gmail.com>
2023-12-20 10:51:59 +0000

    kemajuan lah ini

--- pkg/print.go
+++ pkg/print.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 )
 
@@ -16,26 +17,36 @@ const (
 // PrintDirectoryTree prints the directory structure to the outputFile.
 func PrintDirectoryTree(outputFile *os.File, rootPath string, paths []string) {
 	fmt.Fprintln(outputFile, "Directory Structure:")
+	sort.Strings(paths) // Sort paths to ensure correct order
+	var lastDir string
 	for _, path := range paths {
-		relPath, _ := filepath.Rel(rootPath, path)
-		dirTree := generateDirTree(relPath)
-		fmt.Fprintln(outputFile, dirTree)
-	}
-	fmt.Fprintln(outputFile)
-}
-
-// generateDirTree generates a string representation of the directory tree for a given path.
-func generateDirTree(path string) string {
-	parts := strings.Split(path, string(os.PathSeparator))
-	tree := ""
-	for i, part := range parts {
-		if i == len(parts)-1 {
-			tree += "└── " + part
-		} else {
-			tree += "├── " + part + "\n" + strings.Repeat("│   ", i+1)
+		dir, file := filepath.Split(path)
+		// Print directory if it's different from the last one
+		if dir != lastDir && dir != "" {
+			fmt.Fprintln(outputFile, formatDir(dir, rootPath))
+			lastDir = dir
+		}
+		// Print file
+		if file != "" {
+			fmt.Fprintln(outputFile, formatFile(dir, file, rootPath))
 		}
 	}
-	return tree
+}
+
+// formatDir formats directory string.
+func formatDir(dir, rootPath string) string {
+	relDir, _ := filepath.Rel(rootPath, dir)
+	parts := strings.Split(relDir, string(os.PathSeparator))
+	indent := strings.Repeat("│   ", len(parts)-1)
+	return fmt.Sprintf("%s└── %s [Folder]", indent, parts[len(parts)-1])
+}
+
+// formatFile formats file string.
+func formatFile(dir, file, rootPath string) string {
+	relDir, _ := filepath.Rel(rootPath, dir)
+	parts := strings.Split(relDir, string(os.PathSeparator))
+	indent := strings.Repeat("│   ", len(parts))
+	return fmt.Sprintf("%s└── %s", indent, file)
 }
 
 // PrintFileDetails prints details of a file or directory to the outputFile.
@@ -45,34 +56,41 @@ func PrintFileDetails(rootPath, path string, outputFile *os.File) error {
 		return fmt.Errorf("error getting info for %s: %w", path, err)
 	}
 
-	sizeStr := formatFileSize(info.Size())
-	relPath, _ := filepath.Rel(rootPath, path)
+	printFileInfo(outputFile, rootPath, path, info)
+	return nil
+}
 
+func printFileInfo(outputFile *os.File, rootPath, path string, info os.FileInfo) {
+	relPath, _ := filepath.Rel(rootPath, path)
+	sizeStr := formatFileSize(info.Size())
 	separator := strings.Repeat("-", SeparatorLength)
+
 	fmt.Fprintf(outputFile, "%s\n// The size of (%s): %s\n", separator, relPath, sizeStr)
 	fmt.Fprintf(outputFile, "%s\n// The file location of (%s):\n", separator, relPath)
 	printPath(outputFile, relPath)
 	fmt.Fprintf(outputFile, "%s\n", separator)
 
 	if !info.IsDir() {
-		content, _ := ioutil.ReadFile(path)
-		fmt.Fprintf(outputFile, "\n// The content of (%s):\n", relPath)
-		fmt.Fprintln(outputFile, string(content))
-		fmt.Fprintf(outputFile, "%s\n", separator)
+		content, err := ioutil.ReadFile(path)
+		if err == nil {
+			fmt.Fprintf(outputFile, "\n// The content of (%s):\n", relPath)
+			fmt.Fprintln(outputFile, string(content))
+			fmt.Fprintf(outputFile, "%s\n", separator)
+		}
 	}
-	return nil
 }
 
 // printPath prints the file path in a formatted manner to the outputFile.
 func printPath(outputFile *os.File, relPath string) {
 	dirs := strings.Split(relPath, string(os.PathSeparator))
-	indent := ""
+	var indentBuilder strings.Builder
+
 	for i, dir := range dirs {
 		if i == len(dirs)-1 {
-			fmt.Fprintf(outputFile, "%s└── %s (<-)\n", indent, dir)
+			fmt.Fprintf(outputFile, "%s└── %s (<-)\n", indentBuilder.String(), dir)
 		} else {
-			fmt.Fprintf(outputFile, "%s├── %s\n", indent, dir)
-			indent += "│   "
+			fmt.Fprintf(outputFile, "%s├── %s\n", indentBuilder.String(), dir)
+			indentBuilder.WriteString("│   ")
 		}
 	}
 }
